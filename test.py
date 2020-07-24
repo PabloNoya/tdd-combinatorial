d416fa206f2798551e3ce29f309d5c945f45082f
pablonoya <pablonoya18@live.com>
2020-07-24 09:14:05 -0400

    Test x y n negativos

--- test.py
+++ test.py
@@ -8,3 +8,15 @@ class CombinatorialTests(TestCase):
 
         self.assertGreater(comb.x, comb.n)
         self.assertEqual(comb.calculate(), 0)
+
+    def test_x_negativo(self):
+        comb = Combinatorial(x=-1, n=0)
+
+        self.assertLess(comb.x, 0)
+        self.assertEqual(comb.calculate(), 0)
+
+    def test_n_negativo(self):
+        comb = Combinatorial(x=2, n=-1)
+
+        self.assertLess(comb.n, 0)
+        self.assertEqual(comb.calculate(), 0)
